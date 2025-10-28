aee2a9e71b8ffc66573c862aef8b5b1463c91460
richardpromise <richrdpromise013@gmail.com>
2025-10-28 10:39:26 +0100

    initial push

---
+++ javaScript/app.js
@@ -0,0 +1,53 @@
+// originally javascript was called ECMA SCRIPT
+
+// console
+// varaible declartion
+// data Types
+
+console.log("hello Evidence  good to have you today"); // any variable that has a bracket beside it is called a method
+
+// console.log(1 + 1);
+
+// variable declaration
+let name = "Promise";
+
+//  there are three types of variable declartion key word
+// let
+// const
+// var
+
+// let user = "Admin";
+// const user = "Service provider";
+// var user = "consumer";
+
+// console.log(user);
+
+// let and const are the mordern variable declaration keyword
+// var is the old depreciated variable declaration keyword during the es5 version
+
+// the let keyword allow future change in its value
+// the const keyword does not allow change at all in its value
+
+// name = "Evidence";
+
+// console.log(name);
+// var user = "Admin";
+// let user = 'admin'
+
+// const greetUser = () => {
+//     let user = 'something'
+//   alert("hello user you are welcome to this page ");
+// };
+
+// user = "Loan officer";
+
+// var is a global scope variable declarator and it is also redeclarable
+// let and const are block scope variable declarator its not redeclarable
+
+// console.log(user);
+
+// var user = "testing";
+
+// var user = "james";
+
+// console.log(user);
