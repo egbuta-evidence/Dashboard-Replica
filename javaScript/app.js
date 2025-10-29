68ff0d403a56207c5324bf160346e538ba815a37
richardpromise <richrdpromise013@gmail.com>
2025-10-29 22:30:26 +0100

    update

--- javaScript/app.js
+++ javaScript/app.js
@@ -9,7 +9,7 @@ console.log("hello Evidence  good to have you today"); // any variable that has 
 // console.log(1 + 1);
 
 // variable declaration
-let name = "Promise";
+// let name = "Promise";
 
 //  there are three types of variable declartion key word
 // let
@@ -51,3 +51,52 @@ let name = "Promise";
 // var user = "james";
 
 // console.log(user);
+
+// data types
+// primitiive data types and non primitive data types
+
+// Primitive data types - simple and straightfoward
+// String == anything you put in a single or double quote  " ",, ' '
+
+let name = "12212323230-==@+_=";
+
+console.log(typeof name);
+
+// number
+let value = 32.0;
+console.log(typeof value);
+
+// boolean = is a true or false statement
+
+let example = 4 > 3;
+
+let itsConfirmed = false;
+
+console.log(typeof example);
+
+// undefined ==  when a variable is not assigned to any value
+
+let test;
+
+console.log(typeof test);
+
+// null == empty on pupose or intentional emptiness
+let time = null;
+
+//  bigInt
+let bigNumber = 12234344848949940n;
+bigNumber = BigInt(123454832934823829292939299999);
+
+console.log(typeof bigNumber);
+
+// non - promitive (reference types ) -more complex "eg objects arrays"
+
+// object === ans object is a make up of properties
+
+let person = {
+  name: "Promise",
+  isOutstanding: true,
+  age: 15,
+};
+
+console.log(person.name);
